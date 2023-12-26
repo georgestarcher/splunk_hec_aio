80712212c406c5dc91f13127dff264ddafe12fc6
George Starcher <georgestarcher@users.noreply.github.com>
2023-12-26 10:47:39 -0600

    udpate example.py

--- example.py
+++ example.py
@@ -17,22 +17,22 @@ logging.basicConfig(format='%(asctime)s %(name)s %(levelname)s %(message)s', dat
 log = logging.getLogger(u"MAIN")
 log.setLevel(logging.INFO)
 
-trustHEC = SplunkHecAio("MYINSTANCE.splunkcloud.com","MYTOKEN")
-trustHEC.log.setLevel(logging.INFO)
-trustHEC.set_port(443)
+testHEC = SplunkHecAio("MYINSTANCE.splunkcloud.com","MYTOKEN")
+testHEC.log.setLevel(logging.INFO)
+testHEC.set_port(443)
 
 # Set Index and Sourctype for Post Parameters. Very helpful when using raw data mode.
-trustHEC.set_index("starcher_hec")
-trustHEC.set_sourcetype("aio_json")
+testHEC.set_index("starcher_hec")
+testHEC.set_sourcetype("aio_json")
 
 # Setting Post Limits:
 # We set to maximum number of AIO concurrent POSTs
-trustHEC.set_concurrent_post_limit(20)
+testHEC.set_concurrent_post_limit(20)
 # set_post_max_byte_size: defaults to 512000 (max 800000)
 # We set the smaller 10,000 max size to force data to spread across the AIO concurrent POSTs
-trustHEC.set_post_max_byte_size(10000)
+testHEC.set_post_max_byte_size(10000)
 
-if not trustHEC.check_connectivity():
+if not testHEC.check_connectivity():
     sys.exit(1)
 
 testJSON = {}
@@ -41,12 +41,12 @@ log.info("Starting Data Post")
 for i in range(100000):
     testJSON.update({"event":{"count":i,"name":"dolly bean"}})
     testJSON.update({"time":str(round(time.time(),3))})
-    trustHEC.post_data(testJSON)
+    testHEC.post_data(testJSON)
     payloadLength = len(json.dumps(testJSON))
 
-trustHEC.flush()
+testHEC.flush()
 
-log.info("Completed Data Post: Post Max Size:{0} Max Payload Size:{1} Ratio to Event Size: {2}".format(trustHEC.get_post_max_byte_size(),payloadLength,round(20000/payloadLength,0)))
+log.info("Completed Data Post: Post Max Size:{0} Max Payload Size:{1} Ratio to Event Size: {2}".format(testHEC.get_post_max_byte_size(),payloadLength,round(20000/payloadLength,0)))
 
 # This Example:
 # time python3 main.py
